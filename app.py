fc13383eb3bd300190364d2840c284a2d5681c5d
ffougere <fabrice.fougere@gmail.com>
2026-09-25 15:31:30 -0400

    push update

--- app.py
+++ app.py
@@ -2,11 +2,9 @@ from flask import Flask, jsonify
 
 app = Flask(__name__)
 
-
 @app.get("/")
 def home():
     return jsonify(message="Simple Python app is running")
-
 
 @app.get("/health")
 def health():
